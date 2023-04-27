010d437bc6992583af74813568b534999e6db813
Hugo <cieplucha.hugo@gmail.com>
2023-04-27 16:15:25 +0200

    correctif sql

--- db_login.sql
+++ db_login.sql
@@ -31,8 +31,7 @@ DROP TABLE IF EXISTS `session`;
 CREATE TABLE IF NOT EXISTS `session` (
   `user_id` int NOT NULL,
   `token` varchar(14) COLLATE utf8mb3_bin NOT NULL,
-  `creation_date` date NOT NULL,
-  `expiration_date` date NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `token` (`token`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
@@ -55,15 +54,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `username` (`username`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
-
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`id`, `role`, `username`,`mail`, `password`) VALUES
-(1, 'organisateur', 'Michelo', '', '1234'),
-(2, 'participant', 'Vraistine', '', '2004awe'),
-(3, 'participant', 'Tompu', '', 'lahonte');
 
 --
 -- Contraintes pour les tables déchargées
